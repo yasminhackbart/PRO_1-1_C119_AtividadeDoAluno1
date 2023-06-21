e4f44d2060b4f5e4b40aceb286c814d542804391
Yasmin Hackbart <90115526+yasminhackbart@users.noreply.github.com>
2023-06-21 18:40:07 -0300

    Update train_bot.py

--- train_bot.py
+++ train_bot.py
@@ -56,6 +56,31 @@ stem_words, classes = create_bot_corpus(stem_words,classes)
 print(stem_words)
 print(classes)
 
-# Crie um saco de palavras 
+for word_tags in word_tags_list:
+        
+        bag_of_words = []       
+        pattern_words = word_tags[0]
+       
+        for word in pattern_words:
+            index=pattern_words.index(word)
+            word=stemmer.stem(word.lower())
+            pattern_words[index]=word  
+
+        for word in stem_words:
+            if word in pattern_words:
+                bag_of_words.append(1)
+            else:
+                bag_of_words.append(0)
+        print(bag_of_words)
+
+        labels_encoding = list(labels) #inicialmente, labels será toda zerada
+        tag = word_tags[1] #salve a tag
+        tag_index = classes.index(tag)  #vá para o índice da tag
+        labels_encoding[tag_index] = 1  #anexe 1 àquele índice
+       
+        training_data.append([bag_of_words, labels_encoding])
+
+print(training_data[0])
 
 # Crie os dados de treinamento
+def preprocess_train_data(training_data):
